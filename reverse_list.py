b7395a342f78ddd8ad9f81a16ca37ecbb1882336
Samuel Håkans Persson <samuhaka100@kau.se>
2025-09-09 11:10:55 +0200

    Added explination to reverse_list

--- reverse_list.py
+++ reverse_list.py
@@ -1,3 +1,18 @@
+'''
+Name on Kattis: Öfugsnúið
+
+Jóna needs a program. The program should read in integers and 
+print them in reverse order. Jóna asks for your help.
+
+Input
+The first line contains the integer. Then there is a list of 
+integers, each on their own line. Each integer will be between 
+and.
+
+Output
+Print the list in the reverse order compared to the input.
+'''
+
 n = 0
 
 array = []
